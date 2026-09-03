fc0f62ac4d50fc933b6c4c32cccfcf6ba28fbd73
Darkzveller <elkattoufi.youssef@gmail.com>
2026-09-03 22:39:14 +0200

    test code wifi tester + led bien fonctionnel dans le loop et creation de son fichier exemple

--- git_push.bat
+++ git_push.bat
@@ -1,2 +1,2 @@
 echo off
-git status && git add . && git commit -m "test code led validé et test code wifi tester" && git push
+git status && git add . && git commit -m "test code wifi tester + led bien fonctionnel dans le loop et creation de son fichier exemple" && git push
