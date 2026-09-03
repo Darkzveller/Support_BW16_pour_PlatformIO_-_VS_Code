5f0d19a00315d5b54feb1340b8e5f46b2e2560b8
Darkzveller <elkattoufi.youssef@gmail.com>
2026-09-03 23:09:59 +0200

    Rtos fonctionnelle + auto upload fonctionnel

--- git_push.bat
+++ git_push.bat
@@ -1,2 +1,2 @@
 echo off
-git status && git add . && git commit -m "test code wifi tester + led bien fonctionnel dans le loop et creation de son fichier exemple" && git push
+git status && git add . && git commit -m "Rtos fonctionnelle + auto upload fonctionnel" && git push
